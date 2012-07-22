2907244e7f87fa01daf67b2927af6945e5cd59f2
Spiek <spiek@ymail.com>
2012-07-22 16:10:38 +0200

    Update database structure

--- config/sql/user.sql
+++ config/sql/user.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `state` int(11) NOT NULL DEFAULT '0',
+  `online` bit(1) NOT NULL DEFAULT b'0',
+  `visible` bit(1) NOT NULL DEFAULT b'0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
