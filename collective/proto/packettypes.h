d7636243ec54337c27a5067627dc5ec5d5cd7fbf
Tim Daniel Evert <te@cloudin.de>
2014-01-04 23:46:39 +0100

    packettypes improved

--- collective/proto/packettypes.h
+++ collective/proto/packettypes.h
@@ -9,13 +9,18 @@
 // User Module
 #define PACKET_DESCRIPTOR_USER_LOGIN "user.login"
 #define PACKET_DESCRIPTOR_USER_LOGOUT "user.logout"
-#define PACKET_DESCRIPTOR_USER_SELF_GET_INFO "user.self.getinfo"
+#define PACKET_DESCRIPTOR_USER_SELF_GET_INFO "user.myprofile"
+#define PACKET_DESCRIPTOR_USER_PING "user.ping"
+#define PACKET_DESCRIPTOR_USER_PONG "user.pong"
+
 
 // Contact Module (require User Module)
-#define PACKET_DESCRIPTOR_CONTACT_GET_LIST "contact.getlist"
-#define PACKET_DESCRIPTOR_CONTACT_ALTERED "contact.altered"
+#define PACKET_DESCRIPTOR_CONTACT_GET_LIST "messenger.contactlist"
+#define PACKET_DESCRIPTOR_CONTACT_ALTERED "messenger.contactaltered"
 
 // Chatmanager Module
-#define PACKET_DESCRIPTOR_CHAT_PRIVATE "message.private"
+#define PACKET_DESCRIPTOR_CHAT_PRIVATE "messenger.chatmessage"
+
+#define PACKET_DESCRIPTOR_BULKFILE "messenger.bulkfile"
 
 #endif // PACKETTYPES_H
